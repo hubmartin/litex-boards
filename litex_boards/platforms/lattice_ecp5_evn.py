9d4f8d484c69d53e1581201541c18e2b97dde5c8
hubmartin <hub.martin@gmail.com>
2022-02-05 21:19:47 +0100

    Lattice ECP5 VIP board support
    Signed-off-by: hubmartin <hub.martin@gmail.com>

--- litex_boards/platforms/lattice_ecp5_evn.py
+++ litex_boards/platforms/lattice_ecp5_evn.py
@@ -14,25 +14,24 @@ import os
 
 _io = [
     # Clk / Rst
-    ("clk12",  0, Pins("A10"), IOStandard("LVCMOS33")),
-    ("clk200", 0,
-        Subsignal("p", Pins("Y19")),
-        Subsignal("n", Pins("W20")),
-        IOStandard("LVDS")
-    ),
+    ("clk27",  0, Pins("E17"), IOStandard("LVCMOS33")), ##
+    ("clk100", 0, Pins("C5"), IOStandard("LVDS")), ##
     ("ext_clk50",    0, Pins("B11"), IOStandard("LVCMOS33")),
     ("ext_clk50_en", 0, Pins("C11"), IOStandard("LVCMOS33")),
-    ("rst_n",        0, Pins("G2"),  IOStandard("LVCMOS33")),
+    ("rst_n",        0, Pins("AH1"),  IOStandard("LVCMOS33")),##
 
     # Leds
-    ("user_led", 0, Pins("A13"), IOStandard("LVCMOS25")),
-    ("user_led", 1, Pins("A12"), IOStandard("LVCMOS25")),
-    ("user_led", 2, Pins("B19"), IOStandard("LVCMOS25")),
-    ("user_led", 3, Pins("A18"), IOStandard("LVCMOS25")),
-    ("user_led", 4, Pins("B18"), IOStandard("LVCMOS25")),
-    ("user_led", 5, Pins("C17"), IOStandard("LVCMOS25")),
-    ("user_led", 6, Pins("A17"), IOStandard("LVCMOS25")),
-    ("user_led", 7, Pins("B17"), IOStandard("LVCMOS25")),
+    ("user_led", 0, Pins("AG30"), IOStandard("LVCMOS25")),
+    ("user_led", 1, Pins("AK29"), IOStandard("LVCMOS25")),
+    ("user_led", 2, Pins("AK30"), IOStandard("LVCMOS25")),
+    ("user_led", 3, Pins("AH32"), IOStandard("LVCMOS25")),
+    ("user_led", 4, Pins("AG32"), IOStandard("LVCMOS25")),
+    ("user_led", 5, Pins("AJ29"), IOStandard("LVCMOS25")),
+    ("user_led", 6, Pins("AM28"), IOStandard("LVCMOS25")),
+    ("user_led", 7, Pins("AM29"), IOStandard("LVCMOS25")),
+
+    # ws2812
+    ("ws2812", 0, Pins("AK31"), IOStandard("LVCMOS25")),
 
     # Buttons
     ("user_dip_btn", 1, Pins("J1"),  IOStandard("LVCMOS33")),
@@ -48,21 +47,60 @@ _io = [
 
     # Serial
     ("serial", 0,
-        Subsignal("rx", Pins("P2"), IOStandard("LVCMOS33")),
-        Subsignal("tx", Pins("P3"), IOStandard("LVCMOS33")),
+        Subsignal("rx", Pins("AL28"), IOStandard("LVCMOS25")), ##LVCMOS25
+        Subsignal("tx", Pins("AK28"), IOStandard("LVCMOS25")), ##LVCMOS25
+    ),
+
+    # DDR3 SDRAM
+    ("ddram", 0,
+        Subsignal("a", Pins(
+            " W4  Y5  AB6  P3  AB5  W5  AC6  Y7",
+            "AC7  AD6 W1   Y6  U1   AD7"),
+            IOStandard("SSTL135_I")),
+        Subsignal("ba",    Pins("U3 Y4 W3"), IOStandard("SSTL135_I")),
+        Subsignal("ras_n", Pins("C2"), IOStandard("SSTL135_I")),
+        Subsignal("cas_n", Pins("T1"), IOStandard("SSTL135_I")),
+        Subsignal("we_n",  Pins("P1"), IOStandard("SSTL135_I")),
+        Subsignal("cs_n",  Pins("P2"), IOStandard("SSTL135_I")),
+        Subsignal("dm", Pins("AB3 R6 F2 H6"), IOStandard("SSTL135_I")),
+        Subsignal("dq", Pins(
+            "AC5 AC2 AB4 AE3 W2 AD4 Y1 AB1",
+            "V6  P4  V7  T7  U6 T4  U7 U4",
+            "H2 K1 F1 L2 E1 K3 H3 H1",
+            "N7 J6 L4 K7 P7 L7 K6 H5"),
+            IOStandard("SSTL135_I"),
+            Misc("TERMINATION=75")),
+        Subsignal("dqs_p", Pins("AC3 R4 K2 N3"), IOStandard("SSTL135D_I"),
+            Misc("TERMINATION=OFF"),
+            Misc("DIFFRESISTOR=100")),
+        Subsignal("clk_p", Pins("R3 J4"), IOStandard("SSTL135D_I")),
+        Subsignal("cke",   Pins("T2"), IOStandard("SSTL135_I")),
+        Subsignal("odt",   Pins("V1"), IOStandard("SSTL135_I")),
+        Subsignal("reset_n", Pins("C4"), IOStandard("SSTL135_I")),
+        Misc("SLEWRATE=FAST"),
     ),
 
     # SPIFlash
     ("spiflash", 0,
-        Subsignal("cs_n", Pins("R2"), IOStandard("LVCMOS33")),
-        Subsignal("mosi",   Pins("W2"), IOStandard("LVCMOS33")),
-        Subsignal("miso",   Pins("V2"), IOStandard("LVCMOS33")),
-        Subsignal("wp",     Pins("Y2"), IOStandard("LVCMOS33")),
-        Subsignal("hold",   Pins("W1"), IOStandard("LVCMOS33")),
+        Subsignal("cs_n", Pins("AJ3"), IOStandard("LVCMOS33")),
+        Subsignal("mosi",   Pins("AK2"), IOStandard("LVCMOS33")),
+        Subsignal("miso",   Pins("AJ2"), IOStandard("LVCMOS33")),
+        #Subsignal("wp",     Pins("Y2"), IOStandard("LVCMOS33")),
+        #Subsignal("hold",   Pins("W1"), IOStandard("LVCMOS33")),
     ),
     ("spiflash4x", 0,
         Subsignal("cs_n", Pins("R2"),          IOStandard("LVCMOS33")),
         Subsignal("dq",   Pins("W2 V2 Y2 W1"), IOStandard("LVCMOS33")),
+    ),
+
+    # VGA
+    ("vga", 0,
+        Subsignal("hsync_n", Pins("AJ31")),
+        Subsignal("vsync_n", Pins("AK32")),
+        Subsignal("r", Pins("AM31")),
+        Subsignal("g", Pins("AJ32")),
+        Subsignal("b", Pins("AL32")),
+        IOStandard("LVCMOS25")
     ),
 ]
 
@@ -137,7 +175,8 @@ class Platform(LatticePlatform):
     default_clk_period = 1e9/12e6
 
     def __init__(self, toolchain="trellis", **kwargs):
-        LatticePlatform.__init__(self, "LFE5UM5G-85F-8BG381", _io, _connectors, toolchain=toolchain, **kwargs)
+        #LatticePlatform.__init__(self, "LFE5UM5G-85F-8BG381", _io, _connectors, toolchain=toolchain, **kwargs)
+        LatticePlatform.__init__(self, "LFE5UM-85F-8BG756", _io, _connectors, toolchain=toolchain, **kwargs)
 
     def request(self, *args, **kwargs):
         import time
--- litex_boards/targets/lattice_ecp5_evn.py
+++ litex_boards/targets/lattice_ecp5_evn.py
@@ -17,53 +17,145 @@ from litex_boards.platforms import ecp5_evn
 from litex.soc.cores.clock import *
 from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
-from litex.soc.cores.led import LedChaser
+from litex.soc.integration.soc import SoCRegion
+from litex.soc.cores.led import LedChaser, WS2812
+
+from litedram.modules import MT41J128M16, MT41K64M16
+from litedram.phy import ECP5DDRPHY
+from litex.soc.cores.video import VideoVGAPHY
+
 
 # CRG ----------------------------------------------------------------------------------------------
 
-class _CRG(Module):
-    def __init__(self, platform, sys_clk_freq, x5_clk_freq):
+class _CRG_VERSA(Module):
+    def __init__(self, platform, sys_clk_freq):
         self.rst = Signal()
-        self.clock_domains.cd_sys = ClockDomain()
+        self.clock_domains.cd_init    = ClockDomain()
+        self.clock_domains.cd_por     = ClockDomain(reset_less=True)
+        self.clock_domains.cd_sys     = ClockDomain()
+        self.clock_domains.cd_sys2x   = ClockDomain()
+        self.clock_domains.cd_sys2x_i = ClockDomain(reset_less=True)
 
         # # #
 
-        # clk / rst
-        clk = clk12 = platform.request("clk12")
-        rst_n = platform.request("rst_n")
-        if x5_clk_freq is not None:
-            clk = clk50 = platform.request("ext_clk50")
-            self.comb += platform.request("ext_clk50_en").eq(1)
-            platform.add_period_constraint(clk50, 1e9/x5_clk_freq)
+        self.stop  = Signal()
+        self.reset = Signal()
 
-        # pll
+        # Clk / Rst
+        clk100 = platform.request("clk100")
+        rst_n  = platform.request("rst_n")
+
+        # Power on reset
+        por_count = Signal(16, reset=2**16-1)
+        por_done  = Signal()
+        self.comb += self.cd_por.clk.eq(clk100)
+        self.comb += por_done.eq(por_count == 0)
+        self.sync.por += If(~por_done, por_count.eq(por_count - 1))
+
+        # PLL
         self.submodules.pll = pll = ECP5PLL()
-        self.comb += pll.reset.eq(~rst_n | self.rst)
-        pll.register_clkin(clk, x5_clk_freq or 12e6)
-        pll.create_clkout(self.cd_sys, sys_clk_freq)
+        self.comb += pll.reset.eq(~por_done | ~rst_n | self.rst)
+        pll.register_clkin(clk100, 100e6)
+        pll.create_clkout(self.cd_sys2x_i, 2*sys_clk_freq)
+        pll.create_clkout(self.cd_init, 25e6)
+        self.specials += [
+            Instance("ECLKSYNCB",
+                i_ECLKI = self.cd_sys2x_i.clk,
+                i_STOP  = self.stop,
+                o_ECLKO = self.cd_sys2x.clk),
+            Instance("CLKDIVF",
+                p_DIV     = "2.0",
+                i_ALIGNWD = 0,
+                i_CLKI    = self.cd_sys2x.clk,
+                i_RST     = self.reset,
+                o_CDIVX   = self.cd_sys.clk),
+            AsyncResetSynchronizer(self.cd_sys,   ~pll.locked | self.reset),
+            AsyncResetSynchronizer(self.cd_sys2x, ~pll.locked | self.reset),
+        ]
+
+        #VGA
+        self.clock_domains.cd_vga   = ClockDomain(reset_less=True)
+        pll.create_clkout(self.cd_vga,  25.175e6) # 40e6)
+
 
 # BaseSoC ------------------------------------------------------------------------------------------
 
 class BaseSoC(SoCCore):
+    #mem_map = {**SoCCore.mem_map, **{"spiflash": 0x1000000}}
     def __init__(self, sys_clk_freq=int(50e6), x5_clk_freq=None, toolchain="trellis",
-                 with_led_chaser=True, **kwargs):
+                 with_led_chaser=True, 
+                 with_video_terminal=False,
+                 with_video_framebuffer=True,**kwargs):
         platform = ecp5_evn.Platform(toolchain=toolchain)
+
+        #bios_flash_offset = 0x400000
+
+        # Set CPU variant / reset address
+        #kwargs["cpu_reset_address"] = self.mem_map["spiflash"] + bios_flash_offset
+        kwargs["integrated_rom_size"] = 0x10000
 
         # SoCCore ----------------------------------------------------------------------------------
         SoCCore.__init__(self, platform, sys_clk_freq,
             ident          = "LiteX SoC on ECP5 Evaluation Board",
             ident_version  = True,
+            #integrated_main_ram_size = 0x4000,
+            integrated_main_ram_size = 0,
             **kwargs)
 
         # CRG --------------------------------------------------------------------------------------
-        crg = _CRG(platform, sys_clk_freq, x5_clk_freq)
-        self.submodules.crg = crg
+        crg_cls = _CRG_VERSA
+        self.submodules.crg = crg_cls(platform, sys_clk_freq)
+
+        # DDR3 SDRAM -------------------------------------------------------------------------------
+        if crg_cls == _CRG_VERSA:
+            self.submodules.ddrphy = ECP5DDRPHY(
+                platform.request("ddram"),
+                sys_clk_freq=sys_clk_freq)
+            self.comb += self.crg.stop.eq(self.ddrphy.init.stop)
+            self.comb += self.crg.reset.eq(self.ddrphy.init.reset)
+            self.add_sdram("sdram",
+                phy           = self.ddrphy,
+                module        = MT41K64M16(sys_clk_freq, "1:2"), # Not entirely MT41J64M16 but similar and works(c)
+                l2_cache_size = kwargs.get("l2_size", 8192),
+            )
+
+        # Video ------------------------------------------------------------------------------------
+        if with_video_terminal or with_video_framebuffer:
+            self.submodules.videophy = VideoVGAPHY(platform.request("vga"), clock_domain="vga")
+            if with_video_terminal:
+                self.add_video_terminal(phy=self.videophy, timings="800x600@60Hz", clock_domain="vga")
+            if with_video_framebuffer:
+                #self.add_video_framebuffer(phy=self.videophy, timings="800x600@60Hz", clock_domain="vga")
+                self.add_video_framebuffer(phy=self.videophy, timings="640x480@60Hz", clock_domain="vga")
+                
 
         # Leds -------------------------------------------------------------------------------------
         if with_led_chaser:
             self.submodules.leds = LedChaser(
                 pads         = platform.request_all("user_led"),
                 sys_clk_freq = sys_clk_freq)
+
+        # WS2812
+        self.submodules.ws2812 = WS2812(platform.request("ws2812"), nleds=4, sys_clk_freq=sys_clk_freq)
+        self.bus.add_slave(name="ws2812", slave=self.ws2812.bus, region=SoCRegion(
+            origin = 0x2000_0000,
+            size   = 4*4,
+        ))
+
+        # Running code from SPI flash had some side effects on BIOS with enabled DDR3 memory
+        # So I reverted to the FPGA BRAM for BIOS.
+
+        # # SPI Flash --------------------------------------------------------------------------------
+        # from litespi.modules import MX25L12835F
+        # from litespi.opcodes import SpiNorFlashOpCodes as Codes
+        # self.add_spi_flash(mode="1x", module=MX25L12835F(Codes.READ_1_1_1), with_master=False)
+
+        # # Add ROM linker region --------------------------------------------------------------------
+        # self.bus.add_region("rom", SoCRegion(
+        #     origin = self.mem_map["spiflash"] + bios_flash_offset,
+        #     size   = (16-4)*1024*1024,
+        #     linker = True)
+        # )
 
 # Build --------------------------------------------------------------------------------------------
 
@@ -72,7 +164,7 @@ def main():
     parser.add_argument("--build",        action="store_true", help="Build bitstream")
     parser.add_argument("--load",         action="store_true", help="Load bitstream")
     parser.add_argument("--toolchain",    default="trellis",   help="FPGA toolchain: trellis (default) or diamond")
-    parser.add_argument("--sys-clk-freq", default=60e6,        help="System clock frequency (default: 60MHz)")
+    parser.add_argument("--sys-clk-freq", default=80e6,        help="System clock frequency (default: 60MHz)")
     parser.add_argument("--x5-clk-freq",  type=int,            help="Use X5 oscillator as system clock at the specified frequency")
     builder_args(parser)
     soc_core_args(parser)
